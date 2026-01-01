df64aa84f7420ff9475fccbd1a4fea64c15d3b61
Sameer Patel <samxpatel2@gmail.com>
2026-01-01 07:34:46 +0530

    Models added

--- server/src/models/schema.ts
+++ server/src/models/schema.ts
@@ -4,14 +4,18 @@ const UserSchema = new Schema({
      email: {type:String, required:true, unique:true},
      password:{type:String, required:true},
      role: {type:String,enum:["teacher","student"], required:true},
-})
+});
 const ClassSchema = new Schema({
     className: {type:String, required:true},
     teacherId: {type: mongoose.Schema.Types.ObjectId, ref:'user',required:true},
     studentIds: {type:[mongoose.Schema.Types.ObjectId], ref:'user', default:[]}
-})
+});
 const AttendanceSchema = new Schema({
     classId: {type: mongoose.Schema.Types.ObjectId,ref: 'class', required:true},
     studentId: {type: mongoose.Schema.Types.ObjectId,ref: 'user', required:true},
     status:{type:String, enum:['present', 'absent'] , required:true}
-})
+});
+
+export const UserModel = mongoose.model('user', UserSchema);
+export const ClassModel = mongoose.model('class', ClassSchema);
+export const AttendanceModel = mongoose.model('attendance', AttendanceSchema);
