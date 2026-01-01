d75575bc190bb15d9fea59f4c393c2daad7e6afc
Sameer Patel <samxpatel2@gmail.com>
2026-01-01 07:31:44 +0530

    schema created

---
+++ server/src/models/schema.ts
@@ -0,0 +1,17 @@
+import mongoose , { Schema } from "mongoose";
+const UserSchema = new Schema({
+     name: {type:String, required:true},
+     email: {type:String, required:true, unique:true},
+     password:{type:String, required:true},
+     role: {type:String,enum:["teacher","student"], required:true},
+})
+const ClassSchema = new Schema({
+    className: {type:String, required:true},
+    teacherId: {type: mongoose.Schema.Types.ObjectId, ref:'user',required:true},
+    studentIds: {type:[mongoose.Schema.Types.ObjectId], ref:'user', default:[]}
+})
+const AttendanceSchema = new Schema({
+    classId: {type: mongoose.Schema.Types.ObjectId,ref: 'class', required:true},
+    studentId: {type: mongoose.Schema.Types.ObjectId,ref: 'user', required:true},
+    status:{type:String, enum:['present', 'absent'] , required:true}
+})
